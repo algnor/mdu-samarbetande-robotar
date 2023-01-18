1190319bd4cfc4bb0b9b9eeab9ceb74b726341e1
algnor <algot.nordstrom@gmail.com>
2023-01-18 11:50:30 +0100

    added usb support

--- android_phone/app/src/main/java/com/mdu_robot/android_phone/MainActivity.java
+++ android_phone/app/src/main/java/com/mdu_robot/android_phone/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -17,6 +20,9 @@ import android.widget.ImageView;
 
 import com.google.android.material.slider.RangeSlider;
 import com.google.android.material.slider.Slider;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
 
 import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
@@ -37,6 +43,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+
 public class MainActivity extends CameraActivity {
 
     CameraBridgeViewBase cameraBridgeViewBase;
@@ -47,6 +54,37 @@ public class MainActivity extends CameraActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            Log.d("USBSERIAL", "No driver found");
+            return;
+        }
+
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            Log.d("USBSERIAL", "No connection");
+            return;
+        }
+        UsbSerialPort port = driver.getPorts().get(0);
+        try {
+            port.open(connection);
+            port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_EVEN);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        int i = 0;
+        while (i > 0) {
+            try {
+                i++;
+                port.write("Hello world".getBytes(), 10);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
